8e45fb94884865ebf781ba537154af417091d571
gretchypr <gretchen.bonilla@upr.edu>
2018-11-24 14:33:38 -0400

    Updated sqlSpark code

--- SparkPrograms/SparkMain.py
+++ SparkPrograms/SparkMain.py
@@ -31,7 +31,7 @@ def hashtagExtractor(hashtags):
 
 
 # Gets top 10 trending hashtags in the last 60 minutes
-def getTrendingHashtags(tweets):
+def getTrendingHashtags(tweets, output):
     hashtag_dict = {}
     for tweet in tweets:
         if tweet['hashtags'] != "":
@@ -43,6 +43,7 @@ def getTrendingHashtags(tweets):
                     hashtag_dict[hashtag] = 1
     count = 1
     for hashtag in sorted(hashtag_dict, key=hashtag_dict.get, reverse=True):
+        output.write(str(count) + ". #" + hashtag + " " + str(hashtag_dict[hashtag]) + "\n")
         print(str(count) + ". #" + hashtag + " " + str(hashtag_dict[hashtag]))
         count = count + 1
         if count == 11:
@@ -50,10 +51,10 @@ def getTrendingHashtags(tweets):
 
 
 # Gets top 10 keywords in the last 60 minutes
-def getKeywords(tweets):
+def getKeywords(tweets, output):
     keyword_dict = {}
     # Stop words. Same list as in P1
-    stop_words = ["how", "a", "with", "the", "in", "then", "out",
+    stop_words = ["how", "after", "a", "with", "the", "in", "then", "out",
                   "which", "how's", "what", "when", "what's", "of",
                   "he", "she", "he's", "she's", "this", "that", "but",
                   "by", "at", "are", "and", "an", "as", "am", "i", "i've",
@@ -62,7 +63,7 @@ def getKeywords(tweets):
                   "doesn't", "don't", "doing", "for", "from", "has", "hasn't", "had",
                   "hadn't", "have", "haven't", "him", "her", "he'd", "he'll",
                   "his", "i'm", "i'll", "i'd", "if", "is", "isn't", "it", "it's",
-                  "its", "let's", "or", "other", "she'd", "she'll", "should",
+                  "its", "let's", "or", "on", "other", "she'd", "she'll", "should",
                   "shouldn't", "so", "such", "that's", "they", "they're", "they've",
                   "their", "theirs", "this", "those", "to", "too", "very", "was",
                   "wasn't", "we", "we're", "we've", "we'll", "were", "weren't",
@@ -72,7 +73,7 @@ def getKeywords(tweets):
     for tweet in tweets:
         words = tweet['full_text'].split()
         for word in words:
-            word1 = word.lower()
+            word1 = word.lower().replace("!", "").replace(".", "").replace("?", "").replace("\"", "").strip()
             if word1 not in stop_words and word1 in keyword_dict.keys():
                 keyword_dict[word1] = keyword_dict[word1] + 1
             else:
@@ -80,22 +81,28 @@ def getKeywords(tweets):
     count = 1
     for word in sorted(keyword_dict, key=keyword_dict.get, reverse=True):
         print(str(count) + ". " + word + " " + str(keyword_dict[word]))
+        try:
+            output.write(str(count) + ". " + word + " " + str(keyword_dict[word]) + "\n")
+        except UnicodeEncodeError:
+            output.write(str(count) + ". emoji :) " + str(keyword_dict[word]) + "\n")
         count = count + 1
         if count == 11:
             return
 
 
 # Get count of certain words
-def countWords(tweets):
+def countWords(tweets, output):
     word_dict = {'trump': 0, 'flu': 0, 'zika': 0, 'diarrhea': 0, 'ebola': 0, 'headache': 0, 'measles': 0}
     for tweet in tweets:
         words = tweet['full_text'].split()
         for word in words:
-            word1 = word.lower()
+            word1 = word.lower().replace("'", "").replace("!", "").replace(".", "").replace("?", "")\
+                .replace("\"", "").strip()
             if word1 in word_dict.keys():
                 word_dict[word1] = word_dict[word1] + 1
     count = 1
     for word in sorted(word_dict, key=word_dict.get, reverse=True):
+        output.write(str(count) + ". " + word + " " + str(word_dict[word]) + "\n")
         print(str(count) + ". " + word + " " + str(word_dict[word]))
         count = count + 1
         if count == 11:
@@ -104,6 +111,7 @@ def countWords(tweets):
 initial_data = spark.read.json('/input/output_tweets.json')
 total_rows = initial_data.count()
 tweet_count = 0
+output_file = open('results.txt', 'w')
 
 # Function for converting created_at date string to created timeStamp type
 date_converter = udf(lambda x: datetime.strptime(x[0:20] + x[26:len(x)], "%a %b %d %H:%M:%S %Y"), TimestampType())
@@ -134,37 +142,54 @@ while tweet_count < total_rows:
     tweets_one_hour = tweets_df.select('id', 'hashtags', 'username', 'full_text').filter(tweets_df['created_date'] >= start_created_date).filter(tweets_df['created_date'] <= one_hour_end_date)
     # Get all the tweets in this 12 hour interval
     tweets_twelve_hours = tweets_df.select('id', 'hashtags', 'username', 'full_text').filter(tweets_df['created_date'] >= start_created_date).filter(tweets_df['created_date'] <= twelve_hour_end_date)
-    # Count how many tweets have been handled
-    tweet_count = tweet_count + tweets_one_hour.count()
     # Get list of the texts of each tweet
     tweet_texts = tweets_one_hour.select('full_text').collect()
     if tweets_one_hour.count() != 0:
+        output_file.write("Top ten keywords at " + start_created_date.strftime("%a %b %d %Y %H:%M:%S") + " to "
+                          + one_hour_end_date.strftime("%H:%M:%S") + "\n")
         print("Top ten keywords at " + start_created_date.strftime("%a %b %d %Y %H:%M:%S") + " to "
               + one_hour_end_date.strftime("%H:%M:%S"))
         # Print top ten keywords in the last hour
-        getKeywords(tweet_texts)
+        getKeywords(tweet_texts, output_file)
+        output_file.write("Word occurrences at " + start_created_date.strftime("%a %b %d %Y %H:%M:%S") + " to "
+                          + one_hour_end_date.strftime("%H:%M:%S") + "\n")
         print("Word occurrences at " + start_created_date.strftime("%a %b %d %Y %H:%M:%S") + " to "
               + one_hour_end_date.strftime("%H:%M:%S"))
         # Print the occurence of certain words
-        countWords(tweet_texts)
+        countWords(tweet_texts, output_file)
         # Get list of all the hashtags
         hashtags = tweets_one_hour.select('hashtags').collect()
+        output_file.write("Top ten trending hashtags at " + start_created_date.strftime("%a %b %d %Y %H:%M:%S") + " to "
+                          + one_hour_end_date.strftime("%H:%M:%S") + "\n")
         print("Top ten trending hashtags at " + start_created_date.strftime("%a %b %d %Y %H:%M:%S") + " to "
               + one_hour_end_date.strftime("%H:%M:%S"))
         # Print the top 10 trending hashtags in the last hour
-        getTrendingHashtags(hashtags)
+        getTrendingHashtags(hashtags, output_file)
     if tweets_twelve_hours.count() != 0:
         # Get the users and count them. Order them in ascending order of the number of tweets they have posted
         tweets_twelve_hours.createOrReplaceTempView("tweet")
         users = spark.sql("select username, count(username) as tweets_posted from tweet "
                           "group by username order by tweets_posted")
+        output_file.write("Top ten participants of " + str(users.count()) + " at "
+                          + start_created_date.strftime("%a %b %d %Y %H:%M:%S") + " to "
+                          + twelve_hour_end_date.strftime("%H:%M:%S") + "\n")
         print("Top ten participants of " + str(users.count()) + " at "
               + start_created_date.strftime("%a %b %d %Y %H:%M:%S") + " to "
               + twelve_hour_end_date.strftime("%H:%M:%S"))
         users.show(10)
+        user_list = users.collect()
+        i = 1
+        for user in user_list:
+            output_file.write(str(i) + ". " + user['username'] + " " + str(user['tweets_posted']) + "\n")
+            i = i + 1
+            if i >= 11:
+                break
+    # Count how many tweets have been handled
+    tweet_count = tweet_count + tweets_one_hour.count()
     # Update time intervals
     start_created_date = start_created_date + timedelta(hours=1)
     # One hour after start date
     one_hour_end_date = one_hour_end_date + timedelta(hours=1)
     # 12 hours after start date
     twelve_hour_end_date = twelve_hour_end_date + timedelta(hours=1)
+
